0ad9b891a463c422e3a457769bfb6d395992d63e
tringcooler <tringcooler@users.noreply.github.com>
2017-08-25 17:20:33 +0800

    managed by github desktop

---
+++ py/morphism.py
@@ -0,0 +1,16 @@
+
+from fpgroup import *
+
+class homomorphism(object):
+
+    def __init__(self, src_grp, dst_grp, src_img, dst_img):
+        pass
+
+class isomorphism(homomorphism):
+    pass
+
+class endomorphism(homomorphism):
+    pass
+
+class automorphism(homomorphism):
+    pass
